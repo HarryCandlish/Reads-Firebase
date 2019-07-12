3e87344db2c40ead2ca3d24e781fdba17080ed01
HarryCandlish <hcnd90@gmail.com>
2019-07-12 16:57:50 +1200

    added some images

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,7 +6,17 @@ class Home extends Component {
   render() {
     return (
       <div>
-        <img className={homeStyles.image} alt="godot" src="godot.jpg" />
+        <div>
+          <img className={homeStyles.image} alt="godot" src="godot.jpg" />
+        </div>
+        <div>
+          <img className={homeStyles.bookImage} alt="book" src="book.png" />
+          <img
+            className={homeStyles.pencilImage}
+            alt="pencil"
+            src="pencil.png"
+          />
+        </div>
       </div>
     );
   }
