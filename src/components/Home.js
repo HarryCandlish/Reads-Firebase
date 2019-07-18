a3fc76d510a223ec670074e46d3129c044c6469a
HarryCandlish <hcnd90@gmail.com>
2019-07-18 12:12:41 +1200

    basic design work

--- src/components/Home.js
+++ src/components/Home.js
@@ -11,54 +11,71 @@ class Home extends Component {
         </div>
         <div>
           <ul className={homeStyles.container}>
-            <li className={homeStyles.flexbox}>
-              <img
-                className={homeStyles.imageProfile}
-                alt="profile"
-                src="profile.png"
-              />
-              <h3 className={homeStyles.content}>PROFILE</h3>
-              <p className={homeStyles.paragraph}>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat.
-              </p>
-            </li>
-            <li className={homeStyles.flexbox}>
-              <img className={homeStyles.imageBook} alt="book" src="book.png" />
-              <h3 className={homeStyles.content}>BOOKS</h3>
-              <p className={homeStyles.paragraph}>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat.
-              </p>
-            </li>
-            <li className={homeStyles.flexbox}>
-              <img className={homeStyles.imagePen} alt="pen" src="pencil.png" />
-              <h3 className={homeStyles.content}>REVIEWS</h3>
-              <p className={homeStyles.paragraph}>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat.
-              </p>
-            </li>{" "}
-            <li className={homeStyles.flexbox}>
-              <img
-                className={homeStyles.imageGrowth}
-                alt="growth"
-                src="growth.png"
-              />
-              <h3 className={homeStyles.content}>MONITORING</h3>
-              <p className={homeStyles.paragraph}>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat.
-              </p>
-            </li>
+            <a className={homeStyles.flexbox} href="">
+              <li>
+                <img
+                  className={homeStyles.imageProfile}
+                  alt="profile"
+                  src="profile.png"
+                />
+
+                <h3 className={homeStyles.content}>PROFILE</h3>
+                <p className={homeStyles.paragraph}>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip ex ea commodo consequat.
+                </p>
+              </li>
+            </a>
+            <a className={homeStyles.flexbox} href="">
+              <li>
+                <img
+                  className={homeStyles.imageBook}
+                  alt="book"
+                  src="book.png"
+                />
+                <h3 className={homeStyles.content}>BOOKS</h3>
+                <p className={homeStyles.paragraph}>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip ex ea commodo consequat.
+                </p>
+              </li>
+            </a>
+            <a className={homeStyles.flexbox} href="">
+              <li>
+                <img
+                  className={homeStyles.imagePen}
+                  alt="pen"
+                  src="pencil.png"
+                />
+                <h3 className={homeStyles.content}>REVIEWS</h3>
+                <p className={homeStyles.paragraph}>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip ex ea commodo consequat.
+                </p>
+              </li>{" "}
+            </a>
+            <a className={homeStyles.flexbox} href="">
+              <li>
+                <img
+                  className={homeStyles.imageGrowth}
+                  alt="growth"
+                  src="growth.png"
+                />
+                <h3 className={homeStyles.content}>MONITORING</h3>
+                <p className={homeStyles.paragraph}>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip ex ea commodo consequat.
+                </p>
+              </li>
+            </a>
           </ul>
         </div>
       </div>
