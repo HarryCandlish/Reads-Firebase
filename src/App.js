05d6b0a74825146328630083843d79d01841e644
HarryCandlish <hcnd90@gmail.com>
2019-08-18 11:26:00 +1200

    kno'n

--- src/App.js
+++ src/App.js
@@ -2,15 +2,11 @@ import React from "react";
 import { HashRouter as Router, Route } from "react-router-dom";
 import "./App.css";
 
-import Nav from "./components/Nav";
-import Home from "./components/Home";
-
 function App() {
   return (
-    <Router>
-      <Nav />
-      <Route exact path="/" component={Home} />
-    </Router>
+    <div>
+      <h1>Books</h1>
+    </div>
   );
 }
 
