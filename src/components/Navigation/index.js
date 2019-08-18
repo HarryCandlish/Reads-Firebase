b9797eae5d0f125611792f70d3057fdb29b9339f
HarryCandlish <hcnd90@gmail.com>
2019-08-18 14:56:30 +1200

    auth with firebase

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -38,9 +38,6 @@ const NavigationAuth = () => (
 const NavigationNonAuth = () => (
   <ul>
     <li>
-      <Link to={ROUTES.LANDING}>Landing</Link>
-    </li>
-    <li>
       <Link to={ROUTES.SIGN_IN}>Sign In</Link>
     </li>
   </ul>
